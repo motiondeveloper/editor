6beb954def396dbae4ead24aa5be692336d1c72b
Tim Haywood <tim@haywood.org>
2021-05-18 21:48:07 +1000

    feat: get types from package

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
-import './App.css';
-import Editor from '@monaco-editor/react';
-import defaultCode from './defaultCode';
+import React from "react";
+import "./App.css";
+import Editor from "@monaco-editor/react";
+import defaultCode from "./defaultCode";
+// eslint-disable-next-line import/no-webpack-loader-syntax
+import expressionTypes from "!!raw-loader!expression-globals-typescript/dist/index.d.ts";
 
 function App() {
+  const libCode = expressionTypes.replace(/export /g, "");
+  function handleEditorWillMount(monaco) {
+    monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
+      allowNonTsExtensions: true,
+      noLib: true,
+    });
+
+    monaco.languages.typescript.javascriptDefaults.addExtraLib(
+      `${libCode}
+      const thisComp = new Comp();
+      const thisProperty = new Property<Vector>([0, 0]);
+      const thisLayer = new Layer();`
+    );
+  }
   return (
-    <Editor height="100vh" theme="dark" language="javascript" value={defaultCode} options={{ minimap: {enabled: false} }}/>
+    <Editor
+      height="100vh"
+      theme="vs-dark"
+      language="javascript"
+      value={defaultCode}
+      beforeMount={handleEditorWillMount}
+      options={{ minimap: { enabled: false } }}
+    />
   );
 }
 
--- src/defaultCode.js
+++ src/defaultCode.js
@@ -1,26 +1,9 @@
 const defaultCode = `
-// A basic code editor for expressions
+// A monaco code editor for expressions
+// with docs and autocompletion
 
-// Import eKeys library
-const eKeys = footage('eKeys.jsx').sourceData;
-
-// Create an array of keyframes
-const inKeys = [
-  {
-    keyTime: 1,
-    keyValue: 0,
-    easeIn: 0,
-    easeOut: 66,
-  },{
-    keyTime: 2,
-    keyValue: 250,
-    easeIn: 90,
-    easeOut: 0,
-  }
-];
-
-// Animate
-eKeys.animate(inKeys, time);
-`
+// Hover over values to see the docs!
+const wiggled = thisComp.layer("layer name").transform.position.wiggle(1, 10);
+`;
 
 export default defaultCode;
