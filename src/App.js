c0124f15d0d52acc07d965eb4f623ee63f48a99a
timhaywood <tim@haywood.org>
2021-05-20 11:34:19 +1000

    feat: add saving to url

--- src/App.js
+++ src/App.js
@@ -5,9 +5,38 @@ import defaultCode from "./defaultCode";
 // eslint-disable-next-line import/no-webpack-loader-syntax
 import expressionTypes from "!!raw-loader!expression-globals-typescript/dist/index.d.ts";
 import theme from "./onedarkpro-theme.json";
+import {
+  compressToEncodedURIComponent,
+  decompressFromEncodedURIComponent,
+} from "lz-string";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const libCode = expressionTypes.replace(/export /g, "");
+
+  function loadContentFromUrl() {
+    const params = new URLSearchParams(window.location.search);
+    const encoded = params.get("state");
+
+    if (encoded == null) {
+      return null;
+    }
+
+    const decoded = decompressFromEncodedURIComponent(encoded);
+    if (decoded == null) {
+      throw new Error("failed decompression");
+    }
+    return decoded;
+  }
+
+  const [value, setValue] = React.useState(defaultCode);
+  React.useEffect(() => {
+    const valueFromUrl = loadContentFromUrl();
+    if (valueFromUrl) {
+      setValue(valueFromUrl);
+    }
+  }, []);
+
   function handleEditorWillMount(monaco) {
     // Compiler options
     monaco.languages.typescript.javascriptDefaults.setCompilerOptions({
@@ -45,20 +74,71 @@ function App() {
       },
     });
   }
+
+  function handleEditorDidMount(editor, monaco) {
+    // here is another way to get monaco instance
+    // you can also store it in `useRef` for further usage
+    editor.addAction({
+      // An unique identifier of the contributed action.
+      id: "save-to-url",
+
+      // A label of the action that will be presented to the user.
+      label: "Save to URL",
+
+      // An optional array of keybindings for the action.
+      keybindings: [
+        // chord
+        monaco.KeyMod.chord(monaco.KeyMod.CtrlCmd | monaco.KeyCode.KEY_S),
+      ],
+
+      // A precondition for this action.
+      precondition: null,
+
+      // A rule to evaluate on top of the precondition in order to dispatch the keybindings.
+      keybindingContext: null,
+
+      contextMenuGroupId: "navigation",
+
+      contextMenuOrder: 1.5,
+
+      // Method that will be executed when the action is triggered.
+      // @param editor The editor instance is passed in as a convinience
+      run: async function (editor) {
+        // Encode to URI friendly string
+        const encoded = compressToEncodedURIComponent(editor.getValue());
+        // Replace the current url with a state parameter
+        window.history.replaceState(null, document.title, `?state=${encoded}`);
+        // Copy the URL to the clipboard
+        await navigator.clipboard.writeText(document.location.href);
+        toast.success("Saved to URL and copied to clipboard!", {
+          style: {
+            borderRadius: "16px",
+            color: "#FFFFFF",
+            background: "#282C35",
+          },
+        });
+        return null;
+      },
+    });
+  }
   return (
-    <Editor
-      height="100vh"
-      theme="vs-dark"
-      language="javascript"
-      value={defaultCode}
-      beforeMount={handleEditorWillMount}
-      options={{
-        minimap: { enabled: false },
-        formatOnPaste: true,
-        insertSpaces: false,
-        tabSize: 2,
-      }}
-    />
+    <div>
+      <Toaster position="bottom-center" />
+      <Editor
+        height="100vh"
+        theme="vs-dark"
+        language="javascript"
+        value={value}
+        beforeMount={handleEditorWillMount}
+        onMount={handleEditorDidMount}
+        options={{
+          minimap: { enabled: false },
+          formatOnPaste: true,
+          insertSpaces: false,
+          tabSize: 2,
+        }}
+      />
+    </div>
   );
 }
 
--- src/defaultCode.js
+++ src/defaultCode.js
@@ -4,6 +4,9 @@ const defaultCode = `
 
 // Hover over values to see the docs!
 const wiggled = thisComp.layer("layer name").transform.position.wiggle(1, 10);
-`;
+
+// Press Shift + Alt + F to format code with prettier
+// Press Cntrl/Cmd + S to save the content to the URL for sharing
+`.trim();
 
 export default defaultCode;
